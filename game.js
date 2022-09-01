ade9bfbff68384078d8994593fc1e524a92874fc
Rasmusflorin <rasmus.florin@elev.ga.ntig.se>
2022-09-01 15:27:42 +0200

    Dagens slut

--- game.js
+++ game.js
@@ -32,13 +32,15 @@ let block3 = new Block(1950,700, 60, 60, "red");
 let block4 = new Block(2350,700, 60, 60, "red")
 let blocks = [block,block1,block2,block3,block4];
 
+let count = 0;
+let check = 0;
+
 class Character{
-    constructor(x, y, width, height, color){
+    constructor(x, y, width, height){
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
-        this.color = color;
         this.speedY = 0;
         this.gravity = 0.5;
         this.jumpSpeed = 14;
@@ -48,7 +50,6 @@ class Character{
     drawCharacter(){
         ctx.beginPath();
         ctx.rect(this.x, this.y, this.width, this.height);
-        ctx.fillStyle = this.color;
         ctx.fill();
     }
 
@@ -71,11 +72,45 @@ class Character{
         }
         this.y += this.speedY; 
     }
+    movingCharacter(){
+        if(count == 30){
+            check += 1
+            count = 0;
+        }
+        console.log(count);
+        console.log(check);
+        if(check == 1){
+            var peter1 = new Image();
+            peter1.src = "Peter 2.png";
+            ctx.drawImage(peter1,this.x - 65,this.y + 20,this.width,this.height);  
+        }
+        if(check ==  2){
+            var peter2 = new Image();
+            peter2.src = "Peter 1.png"; 
+            ctx.drawImage(peter2,this.x - 15,this.y + 20,this.width,this.height); 
+        }
+        if(check == 3){
+            var peter3 = new Image();
+            peter3.src = "Peter 3.png"; 
+            ctx.drawImage(peter3,this.x - 15,this.y + 20,this.width,this.height); 
+        }
+        if(check == 4){
+            var peter3 = new Image();
+            peter3.src = "Peter 1.png"; 
+            ctx.drawImage(peter3,this.x - 15,this.y + 20,this.width,this.height);
+        }
+        if(check == 5){
+            var peter3 = new Image();
+            peter3.src = "Peter 1.png"; 
+            ctx.drawImage(peter3,this.x - 100,this.y + 20,this.width,this.height);
+            check = 1; 
+        }
+    }
 }
 
 
 
-let character = new Character(200, 580, 90, 180, "green");
+let character = new Character(200, 580, 100, 180);
 let jump = false;
 
 function collision(b){
@@ -92,9 +127,15 @@ function collision(b){
     
 let pause = true;
 
+
+
 function gameloop() {
     if(pause){ 
         ctx.clearRect(0,0,canvas.width,canvas.height);
+        var background = new Image();
+        background.src = "The_Griffin_House.png";
+        ctx.drawImage(background,0,0,1914,927);  
+        
         for(var i = 0; i < blocks.length; i++){
         
         blocks[i].drawBlock();
@@ -102,14 +143,18 @@ function gameloop() {
         if(collision(blocks[i])) {
                 pause = false;
             } 
-        }  
-        character.drawCharacter();
+        } 
+        //character.drawCharacter();
+        character.movingCharacter(); 
+
+        
         ground.drawBlock();
         character.gravitation();
+        count++;
         window.requestAnimationFrame(gameloop);
     }
     if(!pause){
-        
+        ctx.clearRect(0,0,canvas.width,canvas.height);
     }     
 }
 
