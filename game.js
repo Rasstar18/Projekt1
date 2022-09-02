282923c474d143a63164a128df777c295feb055f
Rasmusflorin <rasmus.florin@elev.ga.ntig.se>
2022-09-02 15:24:42 +0200

    Dagens slut

--- game.js
+++ game.js
@@ -2,6 +2,7 @@
 var canvas = document.getElementById("Canvas");
 var ctx = canvas.getContext("2d");
 
+var again;
 
 class Block{
     constructor(x, y, width, height, color){
@@ -25,32 +26,37 @@ class Block{
 }
 
 let ground = new Block(0,760, 1914, 184, "grey");
-let block = new Block(750,700, 60, 60, "red");
-let block1 = new Block(1150,700, 60, 60, "red");
-let block2 = new Block(1550,700, 60, 60, "red");
-let block3 = new Block(1950,700, 60, 60, "red");
-let block4 = new Block(2350,700, 60, 60, "red")
-let blocks = [block,block1,block2,block3,block4];
+
+let xPos = 500;
+let blocks = [];
+for(let a=0;a<150;a++) {
+    let distance = Math.floor((Math.random() * 500) + 300)
+    xPos += distance;
+    blocks.push(new Block(xPos,700,60,60,"red"));
+}
 
 let count = 0;
 let check = 0;
 
 class Character{
-    constructor(x, y, width, height){
+    constructor(x, y, width, height, color){
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
         this.speedY = 0;
-        this.gravity = 0.5;
+        this.gravity = 0.6;
         this.jumpSpeed = 14;
+        this.color = color;
         this.land = true;
     }
 
     drawCharacter(){
         ctx.beginPath();
         ctx.rect(this.x, this.y, this.width, this.height);
+        ctx.fillStyle = this.color;
         ctx.fill();
+        
     }
 
     jumpCharacter(){
@@ -73,44 +79,89 @@ class Character{
         this.y += this.speedY; 
     }
     movingCharacter(){
-        if(count == 30){
+        let length = 10;
+        //let wide = 30;
+        if(count == 4){
             check += 1
             count = 0;
         }
-        console.log(count);
-        console.log(check);
         if(check == 1){
             var peter1 = new Image();
-            peter1.src = "Peter 2.png";
-            ctx.drawImage(peter1,this.x - 65,this.y + 20,this.width,this.height);  
+            peter1.src = "Peter1.png";
+            ctx.drawImage(peter1,this.x ,this.y + length,this.width,this.height);  
         }
         if(check ==  2){
             var peter2 = new Image();
-            peter2.src = "Peter 1.png"; 
-            ctx.drawImage(peter2,this.x - 15,this.y + 20,this.width,this.height); 
+            peter2.src = "Peter2.png"; 
+            ctx.drawImage(peter2,this.x ,this.y + length,this.width,this.height); 
         }
         if(check == 3){
             var peter3 = new Image();
-            peter3.src = "Peter 3.png"; 
-            ctx.drawImage(peter3,this.x - 15,this.y + 20,this.width,this.height); 
+            peter3.src = "Peter3.png"; 
+            ctx.drawImage(peter3,this.x ,this.y + length,this.width,this.height); 
         }
         if(check == 4){
-            var peter3 = new Image();
-            peter3.src = "Peter 1.png"; 
-            ctx.drawImage(peter3,this.x - 15,this.y + 20,this.width,this.height);
+            var peter4 = new Image();
+            peter4.src = "Peter4.png"; 
+            ctx.drawImage(peter4,this.x ,this.y + length,this.width,this.height);
         }
         if(check == 5){
-            var peter3 = new Image();
-            peter3.src = "Peter 1.png"; 
-            ctx.drawImage(peter3,this.x - 100,this.y + 20,this.width,this.height);
-            check = 1; 
+            var peter5 = new Image();
+            peter5.src = "Peter5.png"; 
+            ctx.drawImage(peter5,this.x,this.y + length,this.width,this.height);
+        }
+        if(check == 6){
+            var peter6 = new Image();
+            peter6.src = "Peter6.png"; 
+            ctx.drawImage(peter6,this.x ,this.y + length,this.width,this.height); 
+        }
+        if(check == 7){
+            var peter7 = new Image();
+            peter7.src = "Peter7.png"; 
+            ctx.drawImage(peter7,this.x ,this.y + length,this.width,this.height); 
+        }
+        if(check == 8){
+            var peter8 = new Image();
+            peter8.src = "Peter8.png"; 
+            ctx.drawImage(peter8,this.x ,this.y + length,this.width,this.height); 
+        }
+        if(check == 9){
+            var peter9 = new Image();
+            peter9.src = "Peter9.png"; 
+            ctx.drawImage(peter9,this.x ,this.y + length,this.width,this.height); 
+        }
+        if(check == 10){
+            var peter10 = new Image();
+            peter10.src = "Peter10.png"; 
+            ctx.drawImage(peter10,this.x ,this.y + length,this.width,this.height); 
+        }
+        if(check == 11){
+            var peter11 = new Image();
+            peter11.src = "Peter11.png"; 
+            ctx.drawImage(peter11,this.x ,this.y + length,this.width,this.height); 
+        }
+        if(check == 12){
+            var peter12 = new Image();
+            peter12.src = "Peter12.png"; 
+            ctx.drawImage(peter12,this.x ,this.y + length,this.width,this.height); 
+        }
+        if(check == 13){
+            var peter13 = new Image();
+            peter13.src = "Peter13.png"; 
+            ctx.drawImage(peter13,this.x ,this.y + length,this.width,this.height); 
+        }
+        if(check == 14){
+            var peter14 = new Image();
+            peter14.src = "Peter1.png"; 
+            ctx.drawImage(peter14,this.x ,this.y + length,this.width,this.height); 
+            check = 1;
         }
     }
 }
 
 
 
-let character = new Character(200, 580, 100, 180);
+let character = new Character(250, 630, 100, 140, "green");
 let jump = false;
 
 function collision(b){
@@ -126,39 +177,77 @@ function collision(b){
 }
     
 let pause = true;
-
-
+let start = false;
+let restart = false;
 
 function gameloop() {
-    if(pause){ 
-        ctx.clearRect(0,0,canvas.width,canvas.height);
-        var background = new Image();
-        background.src = "The_Griffin_House.png";
-        ctx.drawImage(background,0,0,1914,927);  
-        
-        for(var i = 0; i < blocks.length; i++){
-        
-        blocks[i].drawBlock();
-        blocks[i].moveBlock();
-        if(collision(blocks[i])) {
-                pause = false;
-            } 
+    //again = undefined;
+    if(start){
+        if(pause){ 
+            ctx.clearRect(0,0,canvas.width,canvas.height);
+            var background = new Image();
+            background.src = "The_Griffin_House.png";
+            ctx.drawImage(background,0,0,1914,927);  
+            
+            for(var i = 0; i < blocks.length; i++){
+            
+            blocks[i].drawBlock();
+            blocks[i].moveBlock();
+            if(collision(blocks[i])) {
+                    pause = false;
+                }
         } 
-        //character.drawCharacter();
-        character.movingCharacter(); 
-
         
-        ground.drawBlock();
-        character.gravitation();
-        count++;
-        window.requestAnimationFrame(gameloop);
+            character.drawCharacter();
+            character.movingCharacter(); 
+
+            
+            ground.drawBlock();
+            character.gravitation();
+            count++;
+            window.requestAnimationFrame(gameloop);
+        }
+        if(!pause){
+            ctx.clearRect(0,0,canvas.width,canvas.height);
+            ctx.font = "90px Arial";
+            ctx.fillText("Game Over", canvas.width/2, canvas.height/2);
+            ctx.fillStyle = "red";
+            ctx.textAlign = "center";
+            ctx.fillText("Press R to restart", canvas.width/2, 80);
+            ctx.font = "10px Arial";
+            ctx.fillStyle = "red";
+        }  
     }
-    if(!pause){
-        ctx.clearRect(0,0,canvas.width,canvas.height);
-    }     
+    if(!start){
+            ctx.clearRect(0,0,canvas.width,canvas.height);
+            var background = new Image();
+            background.src = "The_Griffin_House.png";
+            ctx.drawImage(background,0,0,1914,927);
+            ctx.font = "30px Arial";
+            ctx.fillText("Press K to start", 800, 80);
+            window.requestAnimationFrame(gameloop);
+    } 
+    if(restart){
+        return(!start);
+    }    
 }
 
 window.onload = gameloop();
+
+/*unction start(){
+    if(!again){
+        again = window.requestAnimationFrame(gameloop);
+    }
+}
+
+function stop(){
+    if(again){
+        window.cancelAnimationFrame(again);
+        again = undefined;
+    }
+}*/
+
+
 
 document.addEventListener('keydown', function(e){
     if(e.key == 'w'){
@@ -169,5 +258,16 @@ document.addEventListener('keydown', function(e){
 document.addEventListener('keyup', function(e){
     if(e.key == 'w'){
         jump = false;
+    }
+})
+
+document.addEventListener('keydown', function(e){
+    if(e.key == 'k'){
+        start = true;
+    }
+})
+document.addEventListener('keydown', function(e){
+    if(e.key == 'r'){
+        restart = true;
     }
 })
