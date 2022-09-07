50dfb4b4f37c7918b59bc765ae62cbff8f9505ff
Rasmusflorin <rasmus.florin@elev.ga.ntig.se>
2022-09-07 15:30:32 +0200

    Dagens slut

--- game.js
+++ game.js
@@ -2,9 +2,15 @@
 var canvas = document.getElementById("Canvas");
 var ctx = canvas.getContext("2d");
 
+let background = new Image();
+background.src = "The_Griffin_House.png";
+
+
 let fast = 0;
 let speed = 0;
 
+
+//***************************************************************************************
 class Block{
     constructor(x, y, width, height, color){
         this.x = x;
@@ -13,79 +19,212 @@ class Block{
         this.height = height;
         this.color = color;
     }
-
     drawBlock(){
         ctx.beginPath();
         ctx.rect(this.x, this.y, this.width, this.height);
         ctx.fillStyle = this.color;
         ctx.fill();
     }
-
     imageBlock(){
         var stone = new Image();
         stone.src = "stone.png";
         ctx.drawImage(stone,this.x + 5,this.y + 5,this.width,this.height);
     }
-
     moveBlock(){
-        if(fast == 50){
+        if(fast == 25){
             speed += 1;
             fast = 0;
         }
         if(speed < 25){
-            this.x-=7;  
-        }
-        if(speed < 50 && speed >= 25){
             this.x-=10;  
         }
+        if(speed < 50 && speed >= 25){
+            this.x-=11;  
+        }
         if(speed < 75 && speed >= 50){
-            this.x-=13;  
+            this.x-=12;  
         }
         if(speed < 100 && speed >= 75){
-            this.x-=16;  
+            this.x-=13;  
         }
         if(speed < 125 && speed >= 100){
-            this.x-=19;  
+            this.x-=14;  
         }
         if(speed < 150 && speed >= 125){
-            this.x-=22;  
+            this.x-=15;  
         }
         if(speed < 175 && speed >= 150){
+            this.x-=16;  
+        }
+        if(speed < 200 && speed >= 175){
+            this.x-=17;  
+        }
+        if(speed < 225 && speed >= 200){
+            this.x-=18;  
+        }
+        if(speed < 250 && speed >= 225){
+            this.x-=19;  
+        }
+        if(speed < 275 && speed >= 250){
+            this.x-=20;  
+        }
+        if(speed < 300 && speed >= 275){
+            this.x-=21;  
+        }
+        if(speed < 325 && speed >= 300){
+            this.x-=22;  
+        }
+        if(speed < 350 && speed >= 325){
+            this.x-=23;  
+        }
+        if(speed < 375 && speed >= 350){
+            this.x-=24;  
+        }
+        if(speed < 400 && speed >= 375){
             this.x-=25;  
         }
-    }
-    
-}
-
-let background = new Image();
-background.src = "The_Griffin_House.png";
-
-let animation = [];
-for(let b=1; b<14; b++) {
-    let frame = new Image();
-    frame.src = "peter"+b+".png";
-    animation.push(frame);
+    }    
 }
 
 let ground = new Block(0,760, 1914, 184, "lightgrey");
 
 
 let blocks;
-function generateObstacles() {
+function generateObstacles(){
     let xPos = 500;
     blocks = [];
 
-    for(let a=0;a<250;a++) {
+    for(let a=0;a<500;a++) {
         let distance = Math.floor((Math.random() * 700) + 500)
         xPos += distance;
         blocks.push(new Block(xPos,700,60,60)); 
     }
 }
 
+//*********************************************************************************************
+class Coin{
+    constructor(x, y, width, height, color){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+    drawCoin(){
+        ctx.beginPath();
+        ctx.rect(this.x, this.y, this.width, this.height);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+    }
+    MoveCoin(){
+        if(fast == 25){
+            speed += 1;
+            fast = 0;
+        }
+        if(speed < 25){
+            this.x-=10;  
+        }
+        if(speed < 50 && speed >= 25){
+            this.x-=11;  
+        }
+        if(speed < 75 && speed >= 50){
+            this.x-=12;  
+        }
+        if(speed < 100 && speed >= 75){
+            this.x-=13;  
+        }
+        if(speed < 125 && speed >= 100){
+            this.x-=14;  
+        }
+        if(speed < 150 && speed >= 125){
+            this.x-=15;  
+        }
+        if(speed < 175 && speed >= 150){
+            this.x-=16;  
+        }
+        if(speed < 200 && speed >= 175){
+            this.x-=17;  
+        }
+        if(speed < 225 && speed >= 200){
+            this.x-=18;  
+        }
+        if(speed < 250 && speed >= 225){
+            this.x-=19;  
+        }
+        if(speed < 275 && speed >= 250){
+            this.x-=20;  
+        }
+        if(speed < 300 && speed >= 275){
+            this.x-=21;  
+        }
+        if(speed < 325 && speed >= 300){
+            this.x-=22;  
+        }
+        if(speed < 350 && speed >= 325){
+            this.x-=23;  
+        }
+        if(speed < 375 && speed >= 350){
+            this.x-=24;  
+        }
+        if(speed < 400 && speed >= 375){
+            this.x-=25;  
+        } 
+    }
+}
+
+
+let coins;
+
+function generateCoins(){
+    let xPosition = 2000;
+    coins = [];
+
+    for(let c=0;c<500;c++) {
+        let dis = Math.floor((Math.random() * 3500) + 2000)
+        xPosition += dis;
+        coins.push(new Coin(xPosition, 500, 30, 30, "#FFDF00")); 
+    }
+}
+
+let point = 0;
+let xLife = 0;
+let pointCheck = 0;
+let coin = false;
+
+function extraLife(){
+    ctx.font = "70px Arial";
+    ctx.fillStyle = "yellow";
+    ctx.fillText("Points: "+point,1750,70);
+
+    for(let p=0; p<coins.length; p++){
+        coins[p].drawCoin();
+        coins[p].MoveCoin();
+
+        if(collision(coins[p])){
+            coins.splice(0,1);
+            point += 1;
+        }
+    }
+
+    if(point >= 10){
+        coin = true;
+        ctx.font = "20px Arial";
+        ctx.fillStyle = "yellow";
+        ctx.fillText("Press P for an extra life", 1750, 100);
+    }
+
+    if(xLife >= 0){
+        ctx.font = "70px Arial";
+        ctx.fillStyle = "yellow";
+        ctx.fillText("Extra lives: "+xLife,200,70);
+    }
+}
+//**************************************************************************
 let count = 0;
 let check = 0;
 
 let playerAlive = true;
+
 
 class Character{
     constructor(x, y, width, height, color){
@@ -100,7 +239,6 @@ class Character{
         this.land = true;
         this.frame = new Image();
     }
-
     drawCharacter(){
         ctx.beginPath();
         ctx.rect(this.x, this.y, this.width, this.height);
@@ -108,7 +246,6 @@ class Character{
         ctx.fill();
         
     }
-
     jumpCharacter(){
         if(this.land){
             this.speedY = -this.jumpSpeed;
@@ -139,7 +276,6 @@ class Character{
         }
 
         this.frame = animation[check];
-
     }
     draw() {
         let lengthy = 35;
@@ -150,10 +286,16 @@ class Character{
     }
 }
 
-
-
 let character = new Character(250, 630, 100, 140, "green");
 let jump = false;
+
+//***********************************************************************
+let animation = [];
+for(let b=1; b<14; b++) {
+    let frame = new Image();
+    frame.src = "peter"+b+".png";
+    animation.push(frame);
+}
 
 function collision(b){
     if(character.x <= b.x + b.width &&
@@ -167,6 +309,7 @@ function collision(b){
     }
 }
 
+//************************************************************************************
 let control = 0;
 let score = 0;
 let highscore = {};
@@ -189,10 +332,11 @@ function drawScore(){
 
     
 }
-    
+
+//***************************************************************************************
 let pause = false;
 
-function gameloop() {
+function gameloop(){
     if(pause){ 
         count++; 
         control++;
@@ -207,21 +351,36 @@ function gameloop() {
         character.draw();
 
         drawScore();
+        extraLife();
+
         
-        for(var i = 0; i < blocks.length; i++){
+        for(let i=0; i<blocks.length; i++){
             blocks[i].moveBlock();
             blocks[i].imageBlock();
 
-            if(collision(blocks[i])) {
-                playerAlive = false; 
-                }
-        } 
-    }
+            if(collision(blocks[i]) && xLife == -1){
+                playerAlive = false;
+            }  
+            
+            
+            if(collision(blocks[i]) && xLife >= 0){
+                pointCheck += 1;
+                playerAlive = true;
+                if(pointCheck >= 10){
+                   xLife -= 1;
+                   pointCheck = 0;  
+                }  
+            } 
+        }
+        console.log(xLife);
+    } 
+        
+
     if(!pause){
         ctx.clearRect(0,0,canvas.width,canvas.height);
         ctx.drawImage(background,0,0,1914,927);
         ctx.font = "30px Arial";
-        ctx.fillText("Press K to start", 800, 80); 
+        ctx.fillText("Press K to start", 950, 80); 
         ctx.textAlign = "center";  
     }   
 
@@ -242,6 +401,8 @@ function gameloop() {
     }
 }
 
+
+//************************************************************************************
 window.onload = function(){
     start();
 };
@@ -249,6 +410,7 @@ window.onload = function(){
 function start(){
     playerAlive = true;
     generateObstacles();
+    generateCoins();
     window.requestAnimationFrame(gameloop);
 }
 
@@ -256,9 +418,14 @@ function start(){
 function restart(){
     score = 0;
     fast = 0;
+    speed = 0;
+    point = 0;
+    xLife ++;
     start();
 }  
 
+
+//********************************************************************************************
 document.addEventListener('keydown', function(e){
     if(e.key == 'r'){
         if(!playerAlive) {
@@ -269,13 +436,13 @@ document.addEventListener('keydown', function(e){
 })   
 
 document.addEventListener('keydown', function(e){
-    if(e.key == 'w'){
+    if(e.key == 'w' || e.key == "ArrowUp" || e.key == " "){
         jump = true;
     }
 })
 
 document.addEventListener('keyup', function(e){
-    if(e.key == 'w'){
+    if(e.key == 'w' || e.key == "ArrowUp" || e.key == " "){
         jump = false;
     }
 })
@@ -283,5 +450,14 @@ document.addEventListener('keyup', function(e){
 document.addEventListener('keydown', function(e){
     if(e.key == 'k'){
         pause = true;
+    }
+})
+document.addEventListener('keydown', function(e){
+    if(e.key == 'p'){
+        if(coin){
+            xLife += 1;
+            point -= 10;
+            coin = false;  
+        }
     }
 })
